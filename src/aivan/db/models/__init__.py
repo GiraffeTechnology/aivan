b9749df62f92c2d30e731482ae5528b8d3057de2
Michael <abcdyi2021@gmail.com>
2026-06-25 04:22:12 +0800

    Register user preference model

--- src/aivan/db/models/__init__.py
+++ src/aivan/db/models/__init__.py
@@ -9,6 +9,7 @@ from aivan.db.models.inquiry import InquiryDraftRecord
 from aivan.db.models.execution import ExecutionEventRecord
 from aivan.db.models.platform import PlatformRecord
 from aivan.db.models.account import OpenClawAccountRecord
+from aivan.db.models.preference import UserPreferenceRecord
 
 __all__ = [
     "Base",
@@ -18,4 +19,5 @@ __all__ = [
     "ExecutionEventRecord",
     "PlatformRecord",
     "OpenClawAccountRecord",
+    "UserPreferenceRecord",
 ]
